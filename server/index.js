664e963a5ae7d5c1d0311c8181625b36b25e5cc2
sid <siddharthsalunkhe2003@gmail.com>
2023-10-31 08:33:37 +0530

    server scripts

--- server/index.js
+++ server/index.js
@@ -9,6 +9,23 @@ const cookieParser = require('cookie-parser')
 const app = express();
 const bcrypt = require('bcryptjs');
 const ws = require('ws');
+const path = require('path');
+
+//=================================================
+// const __dirname1 = path.resolve();
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
+//==========================================================
 
 
 app.use(cors({
